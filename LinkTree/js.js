2c1b102df13a94b3413c12602c35f6ce10728765
Fajar Cahya <71896802+fajarcahya94@users.noreply.github.com>
2022-07-09 01:37:20 +0800

    Add files via upload

---
+++ LinkTree/js.js
@@ -0,0 +1,59 @@
+const link1 = document.querySelector('.link1');
+const link2 = document.querySelector('.link2');
+const link3 = document.querySelector('.link3');
+const link4 = document.querySelector('.link4');
+const link5 = document.querySelector('.link5');
+
+link1.addEventListener('mouseenter', function() {
+	link1.style.backgroundColor = '#fcc801';
+})
+link1.addEventListener('mouseleave', function() {
+	link1.style.backgroundColor = '#250448';
+})
+
+link2.addEventListener('mouseenter', function() {
+	link2.style.backgroundColor = '#fcc801';
+})
+link2.addEventListener('mouseleave', function() {
+	link2.style.backgroundColor = '#250448';
+})
+
+link3.addEventListener('mouseenter', function() {
+	link3.style.backgroundColor = '#fcc801';
+})
+link3.addEventListener('mouseleave', function() {
+	link3.style.backgroundColor = '#250448';
+})
+
+link4.addEventListener('mouseenter', function() {
+	link4.style.backgroundColor = '#fcc801';
+})
+link4.addEventListener('mouseleave', function() {
+	link4.style.backgroundColor = '#250448';
+})
+
+link5.addEventListener('mouseenter', function() {
+	link5.style.backgroundColor = '#fcc801';
+})
+link5.addEventListener('mouseleave', function() {
+	link5.style.backgroundColor = '#250448';
+})
+
+
+
+
+// document.querySelector(".link").addEventListener('mouseenter', masuk);
+// document.querySelector(".link").addEventListener('mouseleave', keluar);
+
+// function masuk (event) {
+// 	event.currentTarget.style.backgroundColor = '#fcc801';
+// }
+
+// function keluar (event) {
+// 	event.currentTarget.style.backgroundColor = '#250448'
+// }
+
+// // let list = document.querySelector("li");
+// // for (i = 0; i < list.length; i++) {
+// // 	list.addEventListener('mouseenter', masuk);
+// // }
